c1d0dbc34c0ca570815bedb5214f55027c5af0da
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2022-09-25 21:50:57 -0600

    Delete 2-CppReview directory

--- 2-CppReview/stdio_cout.cpp
+++
@@ -1,9 +0,0 @@
-// Author: Stephane Dorotich
-// CPSC 457
-// Use freely
-
-#include <iostream>
-
-int main() {
-	std::cout << "String: " << 42 << std::endl;
-}
--- 2-CppReview/stdio_printf.cpp
+++
@@ -1,9 +0,0 @@
-// Author: Stephane Dorotich
-// CPSC 457
-// Use freely
-
-#include <cstdio> // aka <stdio.h>
-
-int main() {
-	printf("String: %d\n", 42);
-}
